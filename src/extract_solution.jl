f0ebb7df9ffec58eb6e51d7963a78a9e3766bba0
Andrey Zakharevich <andreyzakharevich@gmail.com>
2021-10-10 12:06:34 +0300

    Do a proper topological sort when extracting solution

--- src/extract_solution.jl
+++ src/extract_solution.jl
@@ -8,7 +8,7 @@ function extract_solution(branch::SolutionBranch)
         end
     end
     needed_keys = Set([branch.target_key])
-    result = []
+    result_ops = MultiDict{String,ProgramBlock}()
     used_ops = Set()
     while !isempty(needed_keys)
         key = pop!(needed_keys)
@@ -17,14 +17,36 @@ function extract_solution(branch::SolutionBranch)
         end
         op = operations_map[key]
         if !in(op, used_ops)
-            push!(result, operations_map[key])
+            for k in op.inputs
+                insert!(result_ops, k, op)
+            end
             push!(used_ops, op)
             union!(needed_keys, op.inputs)
         end
     end
 
-    output = result[1].p
-    for block in view(result, 2:length(result))
+    result = []
+    ready_to_add = Set()
+    filled_keys = Set()
+    for key in branch.input_keys
+        union!(ready_to_add, result_ops[key])
+        push!(filled_keys, key)
+    end
+    while !isempty(ready_to_add)
+        block = pop!(ready_to_add)
+        push!(result, block)
+        push!(filled_keys, block.output)
+        if block.output != branch.target_key
+            for op in result_ops[block.output]
+                if all(in(k, filled_keys) for k in op.inputs)
+                    push!(ready_to_add, op)
+                end
+            end
+        end
+    end
+
+    output = result[end].p
+    for block in view(result, length(result)-1:-1:1)
         output = LetClause(block.output, block.p, output)
     end
     # This is required because recognition and compression don't support let clauses
@@ -36,11 +58,9 @@ function extract_solution(branch::SolutionBranch)
     return output
 end
 
-replace_free_var(p::Apply, key, v) =
-    Apply(replace_free_var(p.f, key, v), replace_free_var(p.x, key, v))
+replace_free_var(p::Apply, key, v) = Apply(replace_free_var(p.f, key, v), replace_free_var(p.x, key, v))
 
-replace_free_var(p::Abstraction, key, v) =
-    Abstraction(replace_free_var(p.b, key, v))
+replace_free_var(p::Abstraction, key, v) = Abstraction(replace_free_var(p.b, key, v))
 
 replace_free_var(p::LetClause, key, v) =
     LetClause(p.var_name, replace_free_var(p.v, key, v), replace_free_var(p.b, key, v))
@@ -49,8 +69,7 @@ replace_free_var(p::FreeVar, key, v) = p.key == key ? v : p
 
 replace_free_var(p::Program, key, v) = p
 
-beta_reduction(p::LetClause) =
-    beta_reduction(replace_free_var(p.b, p.var_name, p.v))
+beta_reduction(p::LetClause) = beta_reduction(replace_free_var(p.b, p.var_name, p.v))
 
 beta_reduction(p::Program) = p
 

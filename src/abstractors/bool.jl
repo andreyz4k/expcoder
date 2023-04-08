219edb60f051ea874672241dd9757f2e90994d93
Andrey Zakharevich <andreyzakharevich@gmail.com>
2023-04-08 22:22:56 +0300

    Make and and or reversible

--- src/abstractors/bool.jl
+++ src/abstractors/bool.jl
@@ -1,2 +1,34 @@
 
 @define_reverse_primitive "not" arrow(tbool, tbool) (x -> !x) (x -> [!x])
+
+function reverse_and(value)
+    if value
+        return [true, true]
+    else
+        output_options = [[false, true], [true, false], [false, false]]
+        hashed_options = Dict(hash(option) => option for option in output_options)
+        result = []
+        for i in 1:2
+            push!(result, EitherOptions(Dict(h => option[i] for (h, option) in hashed_options)))
+        end
+        return result
+    end
+end
+
+@define_reverse_primitive "and" arrow(tbool, tbool, tbool) (a -> (b -> a && b)) reverse_and
+
+function reverse_or(value)
+    if value
+        output_options = [[false, true], [true, false], [true, true]]
+        hashed_options = Dict(hash(option) => option for option in output_options)
+        result = []
+        for i in 1:2
+            push!(result, EitherOptions(Dict(h => option[i] for (h, option) in hashed_options)))
+        end
+        return result
+    else
+        return [false, false]
+    end
+end
+
+@define_reverse_primitive "or" arrow(tbool, tbool, tbool) (a -> (b -> a || b)) reverse_or

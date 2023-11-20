8ccf8a688cd0c345414c59c1a2c089af8f34149a
Andrey Zakharevich <andreyzakharevich@gmail.com>
2023-11-20 10:23:58 +0200

    Limit maximum list/grid that can be generated by repeat

--- src/abstractors/repeat.jl
+++ src/abstractors/repeat.jl
@@ -7,7 +7,9 @@ function reverse_repeat(value)::Vector{Any}
     return [template_item, length(value)]
 end
 
-@define_reverse_primitive "repeat" arrow(t0, tint, tlist(t0)) (x -> (n -> fill(x, n))) reverse_repeat
+@define_reverse_primitive "repeat" arrow(t0, tint, tlist(t0)) (
+    x -> (n -> n > 1000 ? error("Array is too big") : fill(x, n))
+) reverse_repeat
 
 function reverse_repeat_grid(value)::Vector{Any}
     template_item = first(v for v in value if v !== any_object)
@@ -18,4 +20,6 @@ function reverse_repeat_grid(value)::Vector{Any}
     return [template_item, x, y]
 end
 
-@define_reverse_primitive "repeat_grid" arrow(t0, tint, tint, tgrid(t0)) (x -> (n -> (m -> fill(x, n, m)))) reverse_repeat_grid
+@define_reverse_primitive "repeat_grid" arrow(t0, tint, tint, tgrid(t0)) (
+    x -> (n -> (m -> n * m > 100000 ? error("Grid is too big") : fill(x, n, m)))
+) reverse_repeat_grid

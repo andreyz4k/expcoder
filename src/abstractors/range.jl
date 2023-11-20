0fc7ac48ad1e749e96ab80f4aba02b6b277ed6b9
Andrey Zakharevich <andreyzakharevich@gmail.com>
2023-11-20 14:56:56 +0200

    Limit maximum possible computed range

--- src/abstractors/range.jl
+++ src/abstractors/range.jl
@@ -8,4 +8,6 @@ function reverse_range(value)::Vector{Any}
     return [length(value)]
 end
 
-@define_reverse_primitive "range" arrow(tint, tlist(tint)) (n -> collect(0:n-1)) reverse_range
+@define_reverse_primitive "range" arrow(tint, tlist(tint)) (
+    n -> n > 10000 ? error("Range is too large") : collect(0:n-1)
+) reverse_range

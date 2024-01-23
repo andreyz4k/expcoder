12a248ac1822fd2b7936507f74c444873f8045d4
Andrey Zakharevich <andreyzakharevich@gmail.com>
2024-01-23 16:11:38 +0200

    Set number of julia threads/workers to 2 in CI

--- test/all_tests.jl
+++ test/all_tests.jl
@@ -2,12 +2,12 @@
 using XUnit
 
 @testset runner = DistributedTestRunner() "all" begin
+    include("reachable.jl")
     include("abstractors.jl")
     include("abstractors_tasks.jl")
     include("arc_tasks.jl")
     include("branches.jl")
     include("complexity.jl")
-    include("reachable.jl")
     include("enumeration.jl")
     include("inventions.jl")
     include("objects_processing.jl")

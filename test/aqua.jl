c6d56c9c37a926112b77e999f1a6d436e76a75af
Andrey Zakharevich <andreyzakharevich@gmail.com>
2024-10-15 13:02:04 +0300

    Remove cuDNN from requirements

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,5 +2,5 @@ using Aqua
 using solver
 
 @testcase_log "Aqua.jl" begin
-    Aqua.test_all(solver; deps_compat = false, stale_deps = (ignore = [:CUDA, :cuDNN],), ambiguities = false)
+    Aqua.test_all(solver; deps_compat = false, stale_deps = (ignore = [:CUDA],), ambiguities = false)
 end
